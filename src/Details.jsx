41d7c7e5a989088053b6fd654995e256d595a8cd
dawit belay <dawitalhabesha@gmail.com>
2025-05-19 23:12:56 +0300

    finalizing react query for details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,13 +1,35 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import fetchPet from "./assets/fetchPet";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
     const { id } = useParams();
     const results = useQuery({
-        queryKey: ["details", id],fetchPet,
+        queryKey:["details", id],
+        queryFn:fetchPet,
     });
-    return <h2>{id}</h2>;
+    if(results.isLoading){
+        return (
+            <div className="loading-pane">
+                <h1 className="loader"></h1>
+            </div>
+        );
+    }
+
+    const pet = results.data.pets[0];
+
+    return(
+        <div className = "details">
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>
+                    {pet.animal} - {pet.city}, - {pet.state}
+                    <button>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                </h2>
+            </div>
+        </div>
+    );
   };
   
   export default Details;
